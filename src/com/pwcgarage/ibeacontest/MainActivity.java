17a32d732a35596f38b465550bbef403e2f5a432
asksven <sven.knispel@gmail.com>
2015-04-10 19:01:48 +0200

    initial commit

---
+++ src/com/pwcgarage/ibeacontest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.pwcgarage.ibeacontest;
+
+import com.beaconinside.androidsdk.BeaconService;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		Resources res = getResources();
+		String sdkToken = res.getString(R.string.ibeacon_sdk_id);
+		BeaconService.init(this, "YOUR_TOKEN");
+		
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
